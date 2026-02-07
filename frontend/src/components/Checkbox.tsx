478c850816f8de20b49b9aa5801a5f79e2aaafeb
Mehmet Alperen <delivelialperen@gmail.com>
2026-02-07 23:49:25 +0100

    Add CORS

--- frontend/src/components/Checkbox.tsx
+++ frontend/src/components/Checkbox.tsx
@@ -9,9 +9,9 @@ export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
         return (
             <label className="flex items-center cursor-pointer">
                 <input
-                    ref={ref}
                     type="checkbox"
                     className={`w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500 cursor-pointer ${className}`}
+                    ref={ref}
                     {...props}
                 />
                 {label && (
